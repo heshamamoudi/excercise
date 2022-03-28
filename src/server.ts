6de685adf0b77ac087c261af987a29ef0b96865e
heshamamoudi <hesham69mez@gmail.com>
2022-03-28 20:32:48 +0300

    second commit

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import * as express from 'express'
+import * as bodyParser from 'body-parser'
+
+const app: express.Application = express()
+const address: string = "0.0.0.0:5000"
+
+app.use(bodyParser.json())
+
+app.get('/', function (req: express.Request, res: express.Response) {
+    res.send('Hello World!')
+})
+
+app.listen(3000, function () {
+    console.log(`starting app on: ${address}`)
+})
